2b71479bc7de9094a4c15c0be10f1d46ab4b192a
Софья Прокопенкова <sofyaprokopenkova@yandex.ru>
2024-10-18 12:47:58 +0300

    .egehgirejglkejrkglejrkgljerklg

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-            new Login().Show();
         }
     }
 }
